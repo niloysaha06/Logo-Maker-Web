b92ce8150592dbb94f76e3e6170a303c4da40ba2
niloysaha06 <niloysaha895@gmail.com>
2024-05-19 01:25:42 +0600

    Icon controller & Color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
+import { useState } from "react";
 import "./App.css";
+import BackgroundController from "./components/BackgroundController";
 
 import Header from "./components/Header";
+import IconController from "./components/IconController";
 import SideNav from "./components/SideNav";
 
 function App() {
+  const [selectedIndex, setSelectedIndex] = useState();
   return (
-    <>
+    <div className="h-screen">
       <Header />
       <div className="w-64 fixed">
-        <SideNav selectedIndex={(value) => console.log(value)} />
+        <SideNav selectedIndex={(value) => setSelectedIndex(value)} />
       </div>
-      <div className="ml-64 grid grid-cols-1 md:grid-cols-6 ">
-        <div className="md:col-span-2">control</div>
-        <div className="md:col-span-3">ICon</div>
+      <div className="ml-64 grid grid-cols-1 md:grid-cols-6 fixed">
+        <div className="md:col-span-2 border h-screen shadow-sm p-5 overflow-auto">
+          {selectedIndex == 0 ? <IconController /> : <BackgroundController />}
+        </div>
+        <div className="md:col-span-3"></div>
         <div className="">add</div>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/IconController.jsx
+++ src/components/IconController.jsx
@@ -1,5 +1,69 @@
+import { Smile } from "lucide-react";
+import { Slider } from "@/components/ui/slider";
+import { useEffect, useState } from "react";
+import ColorPickerController from "./ColorPickerController";
+
 const IconController = () => {
-  return <div>IconController</div>;
+  const [size, setSize] = useState(280);
+  const [rotate, setRotate] = useState(0);
+  const [color, setColor] = useState("#fff");
+  const storageValue = JSON.parse(localStorage.getItem("value"));
+  useEffect(() => {
+    const updatedValue = {
+      ...storageValue,
+      iconSize: size,
+      iconRotate: rotate,
+      iconColor: color,
+      icon: "Smile",
+    };
+    localStorage.setItem("value", JSON.stringify(updatedValue));
+  }, [size, rotate, color]);
+  return (
+    <div>
+      <div>
+        <label>Icon</label>
+        <div className="p-3 cursor-pointer bg-gray-200 rounded-md w-[50px] h-[50px] my-2 flex items-center justify-center">
+          <Smile />
+        </div>
+        <div className="py-2">
+          <label className="p-2 flex justify-between items-center">
+            Size
+            <span>{size}</span>
+          </label>
+
+          <Slider
+            defaultValue={[28]}
+            max={512}
+            step={1}
+            onValueChange={(event) => setSize(event[0])}
+          />
+        </div>
+        <div className="py-2">
+          <label className="p-2 flex justify-between items-center">
+            Rotate
+            <span>{rotate}°</span>
+          </label>
+
+          <Slider
+            defaultValue={[0]}
+            max={360}
+            step={1}
+            onValueChange={(event) => setRotate(event[0])}
+          />
+        </div>
+        <div className="py-2">
+          <label className="p-2 flex justify-between items-center">
+            Icon Color
+          </label>
+
+          <ColorPickerController
+            hideController={true}
+            selectedColor={(color) => setColor(color)}
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default IconController;
